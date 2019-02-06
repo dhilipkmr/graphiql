7e107e33397c3e66d8c4da897efa53522a46b0ab
dhilipkmr <dhilip1211@gmail.com>
2019-02-07 00:00:49 +0530

    CRUD mutation

--- schema/schema.js
+++ schema/schema.js
@@ -4,34 +4,50 @@ const {
   GraphQLObjectType, /* Table */
   GraphQLString,
   GraphQLInt,
-  GraphQLSchema
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
 } = graphql;
 
 const companyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
-    id:  { type: GraphQLString},
-    name:  { type: GraphQLString},
-    description:  { type: GraphQLString}
+  fields: () => {
+    return (
+      {
+        id:  { type: GraphQLString},
+        name:  { type: GraphQLString},
+        description:  { type: GraphQLString},
+        user: {
+          type: new GraphQLList(userType),
+          resolve(parentValue, args) {
+            return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`).then((response) => {
+              return response.data;
+            });
+          }
+        }
+      }
+    );
   }
 });
 
 const userType = new GraphQLObjectType({
   name: 'User',
-  fields: {
-    id: { type: GraphQLString},
-    firstName: { type: GraphQLString},
-    age: { type: GraphQLInt},
-    company: {
-      type: companyType,
-      resolve(parentValue, args) {
-        /*Here parentValue gives all the content returned from first query where we Got the users with particular Id then we make another call with that userId to get the details */
-        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`).then((response) => {
-          return response.data;
-        });
+  fields: () => {
+    return {
+      id: { type: GraphQLString},
+      firstName: { type: GraphQLString},
+      age: { type: GraphQLInt},
+      company: {
+        type: companyType,
+        resolve(parentValue, args) {
+          /*Here parentValue gives all the content returned from first query where we Got the users with particular Id then we make another call with that userId to get the details */
+          return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`).then((response) => {
+            return response.data;
+          });
+        }
       }
     }
-  }
+  } 
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -60,7 +76,45 @@ const RootQuery = new GraphQLObjectType({
 /* meaning:you can ask rootquery users in the application, if you give the id of the user i will return the user */
 /* Resolve reaches out and gets the real data */
 
+const mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addUser: {
+      type: userType,
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString)},
+        age:  { type: new GraphQLNonNull(GraphQLInt)},
+        companyId: { type: GraphQLString}
+      },
+      resolve(parentValue, args) {
+        const {firstName, age, companyId = ''} = args;
+        return axios.post('http://localhost:3000/users', {firstName, age, companyId}).then((res) => res.data);
+      }
+    },
+    deleteUser: {
+      type: userType,
+      args: { id: { type: new GraphQLNonNull(GraphQLString)} },
+      resolve(parentValue, args) {
+        const { id } = args;
+        return axios.delete(`http://localhost:3000/users/${id}`).then((res) => res.data);
+      }
+    },
+    editUser: {
+      type: userType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString)},
+        firstName: { type: GraphQLString},
+        age:  { type: GraphQLString},
+        companyId: { type: GraphQLString},
+      },
+      resolve(parentValue, args) {
+        return axios.patch(`http://localhost:3000/users/${args.id}`, args).then((res) => res.data);
+      }
+    }
+  }
+});
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation
 });
 /* It gets the Root Query and returns the instance of user */
